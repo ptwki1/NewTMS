a9329a76d3438579700aa46429c22105ba066e54
Novik Vlad <Vorobushek3333@mail.ru>
2020-04-02 13:09:43 +0300

    lesson13 xml and xsd схема

---
+++ src/lesson13/XSDExample.java
@@ -0,0 +1,31 @@
+package lesson13;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.xml.sax.SAXException;
+
+public class XSDExample {
+    public static void main(String[] args) {
+        boolean answer = validateXMLSchema("test1.xsd", "ComputerXMLDocument.xml");
+        System.out.println("Result:" + answer);
+    }
+
+    public static boolean validateXMLSchema(String xsdPath, String xmlPath) {
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlPath)));
+        } catch (IOException | SAXException e) {
+            System.out.println("Exception: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
