f1ca7840a31b397aa1a4819017deba779433ffa5
Novik Vlad <Vorobushek3333@mail.ru>
2020-04-05 14:28:38 +0300

    update to merge

--- src/lesson12/Ex35.java
+++ src/lesson12/Ex35.java
@@ -6,43 +6,48 @@ import java.io.*;
 //распечатать числа и их среднее арифметическое.
 public class Ex35 {
     public static void main(String[] agrs) throws IOException {
-        DataOutputStream a;
+        DataOutputStream a = null;
 
         try {
             a = new DataOutputStream(new BufferedOutputStream(new
                     FileOutputStream("task35.dat")));
+            for(int i=0; i<20;i++) {
+                Integer random=(int)(Math.random()*100);
+                a.writeInt(random);
+            }
         } catch (FileNotFoundException e) {
             System.out.print("File not found");
             return;
+        }finally {
+            a.close();
         }
-        for(int i=0; i<20;i++) {
-            Integer random=(int)(Math.random()*100);
-            a.writeInt(random);
-        }
-        a.close();
+
         DataInputStream dataInputStream = null;
         try{
             dataInputStream = new DataInputStream(new BufferedInputStream( new
                     FileInputStream("task35.dat")));
+            int res = dataInputStream.readInt();
+            int sum=0;
+            while(true) {
+                System.out.println(res);
+                sum+=res;
+                try {
+                    res = dataInputStream.readInt();
+                } catch (EOFException e) {
+                    break;
+                }
+            }
+            double average=sum/20;
+            System.out.println("Сумма ="+sum);
+            System.out.println("Среднее арефметическое="+average);
+
         }
         catch (FileNotFoundException e) {
             System.out.print("File not found");
             return;
+        }finally {
+            dataInputStream.close();
         }
-        int res = dataInputStream.readInt();
-        int sum=0;
-        while(true) {
-            System.out.println(res);
-            sum+=res;
-            try {
-                res = dataInputStream.readInt();
-            } catch (EOFException e) {
-                break;
-            }
-        }
-        double average=sum/20;
-        System.out.println("Сумма ="+sum);
-        System.out.println("Среднее арефметическое="+average);
-        dataInputStream.close();
+
     }
 }
--- src/lesson12/dopTaskHomework/task4/PageData.java
+++
@@ -1,4 +0,0 @@
-package lesson12.dopTaskHomework.task4;
-
-public class PageData {
-}
